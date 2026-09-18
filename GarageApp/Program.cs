aa7aeec701176889cbc9c94a0a30cb1c0a82e0c1
Blueonn33 <m.marinov1733@gmail.com>
2026-09-18 13:56:17 +0300

    install nuget packages for SQL Server

--- GarageApp/Program.cs
+++ GarageApp/Program.cs
@@ -4,12 +4,17 @@ namespace GarageApp
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+            WebApplication app = builder.Build();
+
+            /*
+               The .Use() methods work with shared resource - HTTP Request
+               Order of execution of .Use() methods matters
+            */
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -20,11 +25,20 @@ namespace GarageApp
             }
 
             app.UseHttpsRedirection();
+
+            /* Enables ASP.NET Core Routing Engine */
+            /* Automatic Routing based on URL Path from HTTP Request */
             app.UseRouting();
 
             app.UseAuthorization();
 
             app.MapStaticAssets();
+
+            /*
+               When configuring more than 1 Route Convention:
+               Map most specific route patterns first 
+               Continue with broader more generalized patterns
+            */
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}")

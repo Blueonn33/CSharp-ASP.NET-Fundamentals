897b59ddfaab7b24441c7326990f39b031144c85
Blueonn33 <m.marinov1733@gmail.com>
2026-09-18 17:12:29 +0300

    constants

---
+++ GarageApp/Common/EntityValidation.cs
@@ -0,0 +1,26 @@
+﻿namespace GarageApp.Common
+{
+    public static class EntityValidation
+    {
+        /* Car Begin */
+        public const int CarMakeMinLength = 1;
+        public const int CarMakeMaxLength = 70;
+
+        public const int CarModelMinLength = 1;
+        public const int CarModelMaxLength = 70;
+
+        public const int CarProductionMonthMinValue = 1;
+        public const int CarProductionMonthMaxValue = 12;
+
+        public const int CarYearMinValue = 1900;
+        /* Car End */
+
+        /* Garage Begin */
+        public const int GarageNameMinLength = 1;
+        public const int GarageNameMaxLength = 100;
+
+        public const int GarageLocationMinLength = 5;
+        public const int GarageLocationMaxLength = 100;
+        /* Garage End */
+    }
+}

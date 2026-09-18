557582a2c179ea53a4a329b0f29d9c5bc0a48941
Blueonn33 <m.marinov1733@gmail.com>
2026-09-18 15:56:03 +0300

    CarType enum

---
+++ GarageApp/Data/Models/Enums/CarType.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable InconsistentNaming
+namespace GarageApp.Data.Models.Enums
+{
+    public enum CarType
+    {
+        Hatchback = 0,
+        Sedan = 1,
+        Coupe = 2,
+        Cabriolet = 3,
+        Tourer = 4,
+        Touring = 5,
+        Roadster = 6,
+        VAN = 7,
+        SUV = 8,
+        UTV = 9
+    }
+}

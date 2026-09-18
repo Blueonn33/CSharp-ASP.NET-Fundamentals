7cad94a6ecd427b043998994a5410b26fe16e317
Blueonn33 <m.marinov1733@gmail.com>
2026-09-19 00:16:32 +0300

    car controller

---
+++ GarageApp/Controllers/CarController.cs
@@ -0,0 +1,54 @@
+﻿using GarageApp.Data;
+using GarageApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GarageApp.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly GarageAppDbContext _dbContext;
+
+        public CarController(GarageAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Car> allCars = _dbContext.Cars
+                .Include(c => c.Garage)
+                .OrderBy(c => c.Make)
+                .ThenBy(c => c.Model)
+                .ThenByDescending(c => c.Year)
+                .ThenByDescending(c => c.ProductionMonth)
+                .Take(25)
+                .ToArray();
+
+            return View(allCars);
+        }
+
+        [HttpGet]
+        public IActionResult Search(string? make)
+        {
+            if (string.IsNullOrEmpty(make))
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            IEnumerable<Car> allCars = _dbContext.Cars
+                .Include(c => c.Garage)
+                //.Where(c => c.Make.ToString().ToLower().Contains(make.ToLower()))
+                .Where(c => EF.Functions.Like(c.Make, $"%{make.ToLower()}%"))
+                .OrderBy(c => c.Make)
+                .ThenBy(c => c.Model)
+                .ThenByDescending(c => c.Year)
+                .ThenByDescending(c => c.ProductionMonth)
+                .Take(25)
+                .ToArray();
+
+            return View(nameof(Index), allCars);
+        }
+    }
+}

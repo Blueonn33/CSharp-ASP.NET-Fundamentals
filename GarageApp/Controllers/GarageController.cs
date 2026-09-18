c04749b86a4da879f966a3ec3298f2caed1212cd
Blueonn33 <m.marinov1733@gmail.com>
2026-09-18 21:34:32 +0300

    Garage controller - index

---
+++ GarageApp/Controllers/GarageController.cs
@@ -0,0 +1,28 @@
+﻿using GarageApp.Data;
+using GarageApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GarageApp.Controllers
+{
+    public class GarageController : Controller
+    {
+        private readonly GarageAppDbContext _dbContext;
+
+        public GarageController(GarageAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Garage> allGarages = _dbContext.Garages
+                .OrderBy(g => g.Name)
+                .ThenBy(g => g.Location)
+                .Take(25)
+                .ToArray();
+
+            return View();
+        }
+    }
+}

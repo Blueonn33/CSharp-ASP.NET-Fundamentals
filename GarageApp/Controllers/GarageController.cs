ca70cbb86e8e7852de3ce4ceae4b2b03bff3a36c
Blueonn33 <m.marinov1733@gmail.com>
2026-09-18 22:38:02 +0300

    details action method

--- GarageApp/Controllers/GarageController.cs
+++ GarageApp/Controllers/GarageController.cs
@@ -1,6 +1,7 @@
 ﻿using GarageApp.Data;
 using GarageApp.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GarageApp.Controllers
 {
@@ -19,12 +20,33 @@ namespace GarageApp.Controllers
             // URL Path is: domain/Garage/Index
             // View Path is: Views/Garage/Index.cshtml
             IEnumerable<Garage> allGarages = _dbContext.Garages
+                .Include(g => g.Cars)
                 .OrderBy(g => g.Name)
                 .ThenBy(g => g.Location)
                 .Take(25)
                 .ToArray();
 
             return View(allGarages);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Глупак");
+            }
+
+            Garage? garageDetails = _dbContext.Garages
+                .Include(g => g.Cars)
+                .SingleOrDefault(g => g.Id == id);
+
+            if (garageDetails == null)
+            {
+                return NotFound("Внимавай какво търсиш");
+            }
+
+            return View(garageDetails);
         }
     }
 }

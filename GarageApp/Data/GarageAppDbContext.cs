594122157bc59ff38259bcc055cb6089f2fca015
Blueonn33 <m.marinov1733@gmail.com>
2026-09-18 18:53:01 +0300

    db context

---
+++ GarageApp/Data/GarageAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GarageApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GarageApp.Data
+{
+    public class GarageAppDbContext : DbContext
+    {
+        public GarageAppDbContext(DbContextOptions<GarageAppDbContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+
+        public virtual DbSet<Car> Cars { get; set; } = null!;
+        public virtual DbSet<Garage> Garages { get; set; } = null!;
+    }
+}

65f8c788a38c599106c9ec9cc5757e606c6b7ff9
Blueonn33 <m.marinov1733@gmail.com>
2026-09-21 21:54:27 +0300

    person view

---
+++ MvcIntro/Controllers/PersonController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcIntro.ViewModels;
+
+namespace MvcIntro.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new PersonViewModel
+            {
+                Id = 1,
+                FirstName = "Martin",
+                LastName = "Marinov",
+                Age = 13
+            };
+
+            return View(model);
+        }
+    }
+}

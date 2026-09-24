febba5988061acb84509af395ef1c441cadcfa91
Blueonn33 <m.marinov1733@gmail.com>
2026-09-24 12:55:40 +0300

    book validation constants

--- BookShelf/Common/EntityValidation.cs
+++ BookShelf/Common/EntityValidation.cs
@@ -5,7 +5,13 @@
         /* Author Begin */
         public const int AuthorNameMinLength = 2;
         public const int AuthorNameMaxLength = 70;
-        public const int AuthorCountryMaxLength = 100; 
+        public const int AuthorCountryMaxLength = 100;
         /* Author End */
+
+
+        /* Book Begin */
+        public const int BookTitleMinLength = 2;
+        public const int BookTitleMaxLength = 120;
+        /* Book End */
     }
 }

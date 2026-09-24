094bece0e7a98fc21e1c7554aa1f1283fbc39776
Blueonn33 <m.marinov1733@gmail.com>
2026-09-24 12:55:28 +0300

    book entity model

---
+++ BookShelf/Data/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static BookShelf.Common.EntityValidation;
+
+namespace BookShelf.Data.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id
+        {
+            get; set;
+        }
+
+        [Required]
+        [MinLength(BookTitleMinLength)]
+        [MaxLength(BookTitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        public int Year
+        {
+            get; set;
+        }
+
+        [ForeignKey(nameof(Author))]
+        public int AuthorId
+        {
+            get; set;
+        }
+
+        public virtual Author Author { get; set; } = null!;
+    }
+}

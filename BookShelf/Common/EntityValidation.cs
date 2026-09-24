65fc2af80edb769234d9590feeb3a2813a0c595a
Blueonn33 <m.marinov1733@gmail.com>
2026-09-24 12:23:09 +0300

    validation constants

---
+++ BookShelf/Common/EntityValidation.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShelf.Common
+{
+    public static class EntityValidation
+    {
+        /* Author Begin */
+        public const int AuthorNameMinLength = 2;
+        public const int AuthorNameMaxLength = 70;
+        public const int AuthorCountryMaxLength = 100; 
+        /* Author End */
+    }
+}

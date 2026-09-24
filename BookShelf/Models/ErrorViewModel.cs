b06aa6999dad95a2a53fa5600fcfc4ec408509ea
Blueonn33 <m.marinov1733@gmail.com>
2026-09-24 11:43:11 +0300

    book shelf project

---
+++ BookShelf/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+namespace BookShelf.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

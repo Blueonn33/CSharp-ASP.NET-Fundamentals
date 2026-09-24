ff0682888a5ac44eb8bb9828df2bbb4308d7beb9
Blueonn33 <m.marinov1733@gmail.com>
2026-09-24 13:39:47 +0300

    authors entity configuration

---
+++ BookShelf/Data/Configuration/AuthorEntityConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using BookShelf.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookShelf.Data.Configuration
+{
+    public class AuthorEntityConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public readonly IEnumerable<Author> SeedAuthors = new List<Author>()
+        {
+            new Author
+            {
+                Id = 1,
+                Name = "Victor Hugo",
+                Country = "France"
+            },
+            new Author
+            {
+                Id = 2,
+                Name = "George Orwell"
+            },
+            new Author
+            {
+                Id = 3,
+                Name = "Алеко Константинов",
+                Country = "България"
+            }
+        };
+
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasData(SeedAuthors);
+        }
+    }
+}

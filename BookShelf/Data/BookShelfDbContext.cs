23beb1bc7c6fd7908d68e63cafc1acaa062d7d02
Blueonn33 <m.marinov1733@gmail.com>
2026-09-24 13:10:33 +0300

    db context

---
+++ BookShelf/Data/BookShelfDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BookShelf.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShelf.Data
+{
+    public class BookShelfDbContext : DbContext
+    {
+        public BookShelfDbContext(DbContextOptions<BookShelfDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public virtual DbSet<Author> Authors
+        {
+            get; set;
+        } = null!;
+
+        public virtual DbSet<Book> Books
+        {
+            get; set;
+        } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}

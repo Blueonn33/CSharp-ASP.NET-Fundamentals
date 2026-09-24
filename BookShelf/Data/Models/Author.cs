117c355f1ccd94fd927ed25f3d4ed88cd7286e79
Blueonn33 <m.marinov1733@gmail.com>
2026-09-24 12:23:01 +0300

    author model

---
+++ BookShelf/Data/Models/Author.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using static BookShelf.Common.EntityValidation;
+
+namespace BookShelf.Data.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id
+        {
+            get; set;
+        }
+
+        [Required]
+        [MinLength(AuthorNameMinLength)]
+        [MaxLength(AuthorNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(AuthorCountryMaxLength)]
+        public string? Country
+        {
+            get; set;
+        }
+    }
+}

8623977b38be5672ac72582d04a6936297b2c415
Blueonn33 <m.marinov1733@gmail.com>
2026-09-24 13:45:08 +0300

    books entity configuration

---
+++ BookShelf/Data/Configuration/BookEntityConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using BookShelf.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookShelf.Data.Configuration
+{
+    public class BookEntityConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public readonly IEnumerable<Book> SeedBooks = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Les Misérables",
+                Year = 1862,
+                AuthorId = 1
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "1984",
+                Year = 1949,
+                AuthorId = 2
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "Бай Ганьо",
+                Year = 1895,
+                AuthorId = 3
+            },
+            new Book
+            {
+                Id = 4,
+                Title = "До Чикаго и назад",
+                Year = 1894,
+                AuthorId = 3
+            }
+        };
+
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(SeedBooks);
+        }
+    }
+}
